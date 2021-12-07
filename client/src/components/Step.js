9babf9f7a24b2ceb78704c563b9803af2ae91dc2
JChris246 <chris.401@live.com>
2021-12-07 14:43:04 -0400

    feat: add welcome page

---
+++ client/src/components/Step.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Step = ({ iconName, caption }) => {
+    const Icon = require("../assets/icons/" + iconName + ".js").default;
+
+    return (
+        <div className="flex flex-col items-center mt-8">
+            <div className="w-20 h-20 p-5 rounded-full bg-gray-700
+                flex justify-center items-center hover:shadow-lg">
+                <Icon width="40" height="40" color="rgb(229, 231, 235)"/>
+            </div>
+            <figcaption className="text-gray-100 my-5">
+                { caption }
+            </figcaption>
+        </div>
+    );
+};
+
+export default Step;
