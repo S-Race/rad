2cc3a082c5c78a12a384f5645d861a94597ddaae
JChris246 <chris.401@live.com>
2021-12-07 12:26:07 -0400

    chore: init client side

---
+++ src/components/SnapContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SnapContainer = ({ children }) => {
+    return (
+        <div className="snapContainer">
+            { children }
+        </div>
+    );
+};
+
+export default SnapContainer;
