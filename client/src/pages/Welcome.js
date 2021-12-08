752c7d8279ff084e70d5d28291eb97b3704fcd92
JChris246 <chris.401@live.com>
2021-12-08 15:01:17 -0400

    fix: CI errors

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -15,13 +15,15 @@ const Welcome = () => {
                     <div className="flex flex-col justify-center items-center min-h-screen blue_gloss">
                         <h1 className="md:text-5xl text-3xl uppercase font-thin my-3">Take your</h1>
                         <h1 className="md:text-5xl text-3xl uppercase font-bold my-3">Music Everywhere</h1>
-                        <h1 className="md:text-5xl text-3xl uppercase font-thin my-3 text-">with <span className="text-blue-700 font-semibold">RAD</span></h1>
+                        <h1 className="md:text-5xl text-3xl uppercase font-thin my-3 text-">
+                            with <span className="text-blue-700 font-semibold">RAD</span></h1>
                     </div>
                 </SnapSection>
                 <SnapSection className="flex bg-blue-800 items-center justify-between px-12">
                     <article className="md:w-2/3 w-2/5">
                         <h2 className="md:text-4xl text-2xl md:my-5 my-2">Your personal music server</h2>
-                        <div className="md:text-lg text-xl text-gray-200 font-thin">Bringing all of your home music together into one place has never been easier.
+                        <div className="md:text-lg text-xl text-gray-200 font-thin">Bringing all of your home music
+                            together into one place has never been easier.
                             Your personal Rad Server streams your music to play in any browser</div>
                     </article>
                     <figure className="w-3/5">
