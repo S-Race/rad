c6835e16e5850be8878a5673ba8aa81ac3ba63e4
JChris246 <chris.401@live.com>
2021-12-11 21:02:16 -0400

    fix: always return the message in a obj with msg attr

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -6,10 +6,14 @@ module.exports.getUser = (req, res) => {
     User.findOne({ username: req.params.name }, (err, user) => {
         if (err) {
             console.log(err);
-            res.status(500).send(err);
+            res.status(500).send({
+                msg: err
+            });
         }
         if (!user)
-            res.status(404).send("User not found");
+            res.status(404).send({
+                msg: "User not found"
+            });
         else res.status(200).send({
             msg: "User logged in successfully as " + user.username,
             username: user.username,
@@ -18,26 +22,32 @@ module.exports.getUser = (req, res) => {
     });
 };
 
-// Get user
+// create user
 module.exports.createUser = (req, res) => {
     const username = req.params.name;
     User.find({ username }, (err, user) => {
         if (err) {
             console.log(err);
-            res.status(500).send(err);
+            res.status(500).send({
+                msg: err
+            });
         }
         if (user?.length < 1) {
             const newUser = new User({ username });
             newUser.save().then(savedUser => {
                 if (savedUser === newUser) // if savedUser returned is the same as newUser the saved successfully
                     res.status(201).send({
-                        username: user.username,
+                        username: savedUser.username,
                         id: user._id,
                         msg: "User created successfully",
                     });
-                else res.status(500).send("User created failed");
+                else res.status(500).send({
+                    msg: "User created failed"
+                });
             });
         }
-        else res.status(409).send("User with that username already exists");
+        else res.status(409).send({
+            msg: "User with that username already exists"
+        });
     });
 };
