d08a1dd1d6a4e73adc1e5bf415091575f2ade02a
JChris246 <43832407+JChris246@users.noreply.github.com>
2021-12-26 16:12:43 -0400

    [Feature] Search library and playlists (#26)
    * extract control of music player to custom hook ... and remove favicon request
    
    * feat: implement searching library and playlists

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useOutletContext } from "react-router-dom";
 
 import { useUserContext } from "../UserContext";
+import { useSearchContext } from "../SearchContext";
 
 import Loader from "../components/Loader";
 import PlaylistItem from "../components/PlaylistItem";
@@ -10,30 +10,31 @@ import PlaylistItem from "../components/PlaylistItem";
 const Playlists = () => {
     const [playlistsLoaded, setPlaylistsLoaded] = useState(false);
     const [playlists, setPlaylists] = useState([]);
+
     const { user } = useUserContext();
     const navigate = useNavigate();
+    const { search } = useSearchContext();
 
     const onItemClick = id => navigate("/playlist/" + id);
 
-    useEffect(() => {
-        // get list of playlists to provide options to add
-        (async () => {
-            const res = await fetch("/api/playlists/" + user.id, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+    const getResults = async (query) => {
+        const res = await fetch("/api/playlists/" + user.id + (query?.length > 1 ? "?search=" + query : ""), {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-            if (res.status !== 200) {
-                alert("A critical error occurred");
-                return;
-            }
+        if (res.status !== 200) {
+            alert("A critical error occurred");
+            return;
+        }
 
-            setPlaylists(await res.json());
-            setPlaylistsLoaded(true);
-        })();
-    }, []);
+        setPlaylists(await res.json());
+        setPlaylistsLoaded(true);
+    };
+
+    useEffect(() => getResults(search), [search]);
 
     return (
         <div className="bg-gray-800 text-gray-100 min-h-screen p-5">
